99836724ce5959c45a898b88a828d44e356ce42c
NguyenQuyThien <dev.thien@gmail.com>
2025-07-05 11:46:38 +0700

    add: entities

---
+++ TestValidateApp/Domain/Entities/Stream.swift
@@ -0,0 +1,37 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+A simple class that represents an entry from the `Streams.plist` file in the main application bundle.
+*/
+
+import Foundation
+
+struct Stream: Codable, Sendable {
+    
+    // MARK: Types
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "name"
+        case playlistURL = "playlist_url"
+    }
+    
+    // MARK: Properties
+    
+    /// The name of the stream.
+    let name: String
+    
+    /// The URL pointing to the HLS stream.
+    let playlistURL: String
+  
+  init(name: String, playlistURL: String) {
+    self.name = name
+    self.playlistURL = playlistURL
+  }
+}
+
+extension Stream: Equatable {
+    static func ==(lhs: Stream, rhs: Stream) -> Bool {
+        return (lhs.name == rhs.name) && (lhs.playlistURL == rhs.playlistURL)
+    }
+}
